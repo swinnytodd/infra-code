b5e877245e86354cad94725d2b75f5cba40835c3
marvin4ward <marvin4ward@gmail.com>
2022-08-18 23:48:01 +0700

    Revert "Adding new user - prototype"

--- terraform/iam/iam_users.tf
+++ terraform/iam/iam_users.tf
@@ -1,4 +1,16 @@
-resource "aws_iam_user" "prototype" {
-  name = "prototype"
+# file for iam users
+
+resource "aws_iam_user" "valentin" {
+  name = "valentin"
+  path = "/users/"
+}
+
+resource "aws_iam_user" "swinnytodd" {
+  name = "swinnytodd"
+  path = "/users/"
+}
+
+resource "aws_iam_user" "txd3h" {
+  name = "txd3h"
   path = "/users/"
 }
